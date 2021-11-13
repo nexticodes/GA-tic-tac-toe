5622607d5744ea9c06505fc3667940c375a4e22f
Ernest Bruno <erndobruno@gmail.com>
2021-11-12 21:51:00 -0800

    Works on click handler function, adds flipTurn helper function. Also works on initializing global variables on init().

--- js/main.js
+++ js/main.js
@@ -14,39 +14,39 @@
 
 // CONSTANTS
 // store colors the players want their respective colors to be; [blue, red, gray]
-const colors;
+const colors = [];
 // winning conditions array;
-const winningConditions;
+let winningConditions;
 
 // VARIABLES
-const boardArr; //  = [0,0,0,0,0,0,0,0,0];
-const turnCurr; // player 1 or 2.
-const turnNum1; // number of turns.
-const turnNum2;
-const winner; // the winner;
+let boardArr = []; //  = [0,0,0,0,0,0,0,0,0];
+let turnCurr; // player 1 or 2.
+let turnNum1; // number of turns.
+let turnNum2;
+let winner; // the winner;
+let boardEl;
 
 // Initialize game 
-const player1 = 'Player 1' // (prompt('Player 1. Enter your name: '));
-const player2 = 'Player 2' // (prompt('Player 2. Enter your name: ')) ;
+let player1 = 'Player 1' // (prompt('Player 1. Enter your name: '));
+let player2 = 'Player 2' // (prompt('Player 2. Enter your name: ')) ;
+
 
 // SET THE STAGE
 const init = () => {
     // fill up variables;
     // set boardArray, set random first turn (use Math.random()), number of turns for 1 and 2. Set winner to 0.
-    // add event listeners to the cells.
+    boardArr = [0,0,0,0,0,0,0,0,0];
+    turnCurr = Math.floor(Math.random() * 2 + 1);
+    // add event listeners to the board.
+    boardEl = document.querySelector('#board');
+    boardEl.addEventListener('click', handleClick);
     // REMEMBER BUBBLING. Apply listener to board component.
     // Maybe make a loop instead? Hmm... 
     // Play the game.
-};
 
-// event listener 1. Handle click.
-// Extract the index (VALUE or maybe ID) of the cell.
-// If it's player 1's turn, replace cell with X.
-// If it's player 2's turn, replace cell with O.
-// Fill board array at extracted index with 1 or 2, depending on whose turn it is.
-// MAKE CELL UNCLICKABLE.
-// CHECK FIRST IF VALUE AT INDEX IS EITHER 1 OR 2.
-// Check win condition.
+};
+init();
+console.log(boardArr);
 
 
 // play function.
@@ -56,3 +56,32 @@ const init = () => {
 // Wait for player 2's turn or click.
 // Add 1 to turnNum2.
 // Keep going while there is no winner.
+
+// event listener 1. Handle click.
+function handleClick(e){
+    // Extract the index (VALUE or maybe ID) of the cell.
+    const clickedIndex = e.target.id;
+    // Fill board array at extracted index with 1 or 2, depending on whose turn it is.
+    // Check first if taken.
+    const arrIndexValue = boardArr[clickedIndex];
+    console.log("Value at array index ", arrIndexValue);
+    boardArr[clickedIndex] = turnCurr;
+    console.log(boardArr);
+    console.log('Before flip ', turnCurr);
+    flipTurn();
+    console.log('After flip ', turnCurr);
+    // Check win condition.
+}
+
+// flip turn
+function flipTurn(){
+    // negate.
+    turnCurr = !turnCurr;
+};
+
+// render board function when a click is made.
+// If it's player 1's turn, replace cell with X.
+// If it's player 2's turn, replace cell with O.
+// MAKE CELL UNCLICKABLE.
+// CHECK FIRST IF VALUE AT INDEX IS EITHER 1 OR 2.
+
